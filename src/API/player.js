6d7d9eeaf793b2b972d704e5301bfed41e2bf9db
Aviv2991 <55682481+Aviv2991@users.noreply.github.com>
2019-12-25 13:30:39 +0200

    first function build

--- src/API/player.js
+++ src/API/player.js
@@ -1,14 +1,16 @@
 class Player {
     constructor(color) {
         this.color = color;
+        this.turnCounter = 0;
     }
     insertToken = (colIndex) => { return Boolean };
 }
 
-// class ComputerPlayer extends Player{
-//     constructor(color){
-//        super(color);
-//     }
-//     insertToken = (colIndex) =>{return Boolean};
-// }
+class ComputerPlayer extends Player{
+    constructor(color){
+       super(color);
+    }
+    insertToken = () =>{return Boolean};
+}
 
+export {Player,ComputerPlayer}
